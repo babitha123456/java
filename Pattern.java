4931ec623c49aeeee7df5a3cc33fd183f4870a96
babitha123456 <113047181+babitha123456@users.noreply.github.com>
2022-09-28 13:28:24 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,30 @@
+//Babitha 
+class Pattern
+
+{ 
+
+public static void main(String[]args)
+
+{
+
+int j, rows=6;
+
+for(int i=rows-1;i>0;i--)
+
+{
+
+for(j=1;j<=i;j++)
+
+{
+
+System.out.println("*" +" ");
+
+}
+
+System.out.println();
+
+}
+
+}
+
+}
