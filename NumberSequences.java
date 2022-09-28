4ebb9c9359c34b5c00b3923239edd3ae61b6cd3f
babitha123456 <113047181+babitha123456@users.noreply.github.com>
2022-09-28 13:29:58 +0530

    Add files via upload

---
+++ NumberSequences.java
@@ -0,0 +1,35 @@
+//Babitha 
+class NumberSequences
+
+{
+
+public static void main(String[]args)
+
+{
+
+int a = 1;
+
+int b = 4;
+
+int c = 7;
+
+int n = 10;
+
+for(int i = 1; i <= n; i++)
+
+{
+
+int d = a + b + c;
+
+System.out.println(a + " " + b + " " + c + " " + d );
+
+System.out.println(" ");
+
+a = d;
+
+b = b + 1;
+ }
+ 
+ }
+ 
+ }
