4a49dcf9bd0d89d5cecc88fb0fa5a99d2f868d0f
babitha123456 <113047181+babitha123456@users.noreply.github.com>
2022-09-28 12:55:46 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,25 @@
+//Babitha java batch 2576
+class Variable
+{
+	int a=10;//instance variable		
+	
+	static int b=20;//static variable	
+	
+	public int m1()
+	{
+		int c = 25;//local variable	
+		
+		Sysytem.out.println("c="+c);
+		
+		return 0;
+		
+	}
+	public static void main(Syting[]args)
+	{
+		VariableType variable= new VariabeType();
+		
+		System.out.println("a="+variable.a+"+b);
+		
+		variable.m1();
+	}
+}
