059d22eee3df8e78f7b7e86e7df8bf2f2f70b9c9
babitha123456 <113047181+babitha123456@users.noreply.github.com>
2022-10-21 12:20:31 +0530

    Add files via upload

---
+++ Bankcustomer2.java
@@ -0,0 +1,11 @@
+//package name "anudip1"
+package anudip1;//package creation
+import anudip.*;
+public class Bankcustomer2
+{
+	public static void main(String[] args)//main method
+	{
+		Bankcustomer2 obj=new Bankcustomer2();//object creation
+		 obj.show();//call the method
+	}
+}
